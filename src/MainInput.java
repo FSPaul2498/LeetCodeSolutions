144e7941569d70a15ceb248867abf59b811d6bed
FSPaul <baixu2498@foxmail.com>
2021-08-01 15:27:12 +0800

    Improve MainInput class and solve Q1

--- src/MainInput.java
+++ src/MainInput.java
@@ -7,9 +7,45 @@ import java.util.Arrays;
 
 public class MainInput {
     public static void main(String[] args) {
-        // Q1337. 矩阵中战斗力最弱的 K 行
-        // the-k-weakest-rows-in-a-matrix
-        run1337();
+        int option;
+        try {
+            option = Integer.parseInt(args[0]);
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+            option = 0;
+        }
+        print("Solve Question #" + option);
+        switch (option) {
+            case 1:
+                // Q1. 两数之和
+                // two-sum
+                run1();
+                break;
+            case 1337:
+                // Q1337. 矩阵中战斗力最弱的 K 行
+                // the-k-weakest-rows-in-a-matrix
+                run1337();
+                break;
+            default:
+                print("Wrong Question Num");
+        }
+    }
+
+    static void run0() {
+        /*
+         This is a Method Template
+         Initialize Variables
+         S0 run = new S0();
+         int result = run.methods();
+         print(result);
+        */
+    }
+
+    static void run1() {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        S1 run = new S1();
+        int[] result = run.twoSum(nums, target);
+        print(result);
     }
 
     static void run1337() {
@@ -31,5 +67,9 @@ public class MainInput {
 
     static void print(int[] input) {
         System.out.println(Arrays.toString(input));
+    }
+
+    static void print(String input) {
+        System.out.println(input);
     }
 }

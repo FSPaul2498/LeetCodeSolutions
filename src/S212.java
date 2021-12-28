68a4d3afe697bc5afdd70d297ae13179369f8b9f
FSPaul <baixu2498@foxmail.com>
2021-12-28 14:10:52 +0800

    Rename P212 class "Trie" to "Trie212" to avoid conflicts

--- src/S212.java
+++ src/S212.java
@@ -7,7 +7,7 @@ public class S212 {
     int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     public List<String> findWords(char[][] board, String[] words) {
-        Trie trie = new Trie();
+        Trie212 trie = new Trie212();
         for (String word : words)
             trie.insert(word);
         HashSet<String> ans = new HashSet<>();
@@ -17,7 +17,7 @@ public class S212 {
         return new ArrayList<>(ans);
     }
 
-    public void dfs(char[][] board, Trie now, int i1, int j1, HashSet<String> ans) {
+    public void dfs(char[][] board, Trie212 now, int i1, int j1, HashSet<String> ans) {
         if (!now.children.containsKey(board[i1][j1])) return;
         char ch = board[i1][j1];
         now = now.children.get(ch);
@@ -26,28 +26,26 @@ public class S212 {
         board[i1][j1] = '#';
         for (int[] dir : dirs) {
             int i2 = i1 + dir[0], j2 = j1 + dir[1];
-            if (i2 >= 0 && i2 < board.length && j2 >= 0 && j2 < board[0].length)
-                dfs(board, now, i2, j2, ans);
+            if (i2 >= 0 && i2 < board.length && j2 >= 0 && j2 < board[0].length) dfs(board, now, i2, j2, ans);
         }
         board[i1][j1] = ch;
     }
 }
 
-class Trie {
+class Trie212 {
     String word;
-    HashMap<Character, Trie> children;
+    HashMap<Character, Trie212> children;
 
-    public Trie() {
+    public Trie212() {
         this.word = "";
         this.children = new HashMap<>();
     }
 
     public void insert(String word) {
-        Trie cur = this;
+        Trie212 cur = this;
         for (int i = 0; i < word.length(); ++i) {
             char c = word.charAt(i);
-            if (!cur.children.containsKey(c))
-                cur.children.put(c, new Trie());
+            if (!cur.children.containsKey(c)) cur.children.put(c, new Trie212());
             cur = cur.children.get(c);
         }
         cur.word = word;
